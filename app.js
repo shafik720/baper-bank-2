415d0bf449c7df67614c6ac04db8c14abf2d116f
shafik720 <shafirkasel5@gmail.com>
2022-08-14 03:07:18 +0600

    sdfsdf

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 
 
-let name = ['Where have all the flowers gone , young girls picked them everyone'];
+document.getElementById('submittBtn').addEventListener('click',function(){
+    let names = document.getElementById('name');
+    let name = names.value;
+    let emails = document.getElementById('email');
+    let email = emails.value; 
 
-for(let i=0; i<10; i++){
-    console.log(i);
-}
+    let errorForName = document.getElementById('nameError');
+    
+    if(name==''){
+        errorForName.style.display = 'block';
+        errorForName.innerText = 'You must type something !';
+    }else if(isNaN(name) ==false){
+        errorForName.style.display = 'block';
+        errorForName.innerText = 'Numbers not allowed';
+    }else if(name.toLowerCase() != 'rafayel'){
+        errorForName.style.display = 'block';
+        errorForName.innerText = 'Wrong Name';
+    }else if(email==''){
+        
+    }
+
+    names.value= '';
+})
