803def55c0807446aa796c759395b7f8a41c81c3
shafik720 <shafirkasel5@gmail.com>
2022-08-14 03:18:22 +0600

    sdfasaf

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.getElementById('submittBtn').addEventListener('click',function(){
     let email = emails.value; 
 
     let errorForName = document.getElementById('nameError');
+    let errorForEmail = document.getElementById('emailError');
     
     if(name==''){
         errorForName.style.display = 'block';
@@ -18,8 +19,21 @@ document.getElementById('submittBtn').addEventListener('click',function(){
         errorForName.style.display = 'block';
         errorForName.innerText = 'Wrong Name';
     }else if(email==''){
-        
+        errorForName.style.display = 'none';
+        errorForEmail.style.display = 'block';
+        errorForEmail.innerText = 'Empty field not allowed ';
+    }else if(isNaN(email) == false){
+        errorForName.style.display = 'none';
+        errorForEmail.style.display = 'block';
+        errorForEmail.innerText = 'Number not allowed ';
+    }else if(email != 'email@gmail.com'){
+        errorForName.style.display = 'none';
+        errorForEmail.style.display = 'block';
+        errorForEmail.innerText = 'Invalid email ! ';;
+    }
+    else if(name=='rafayel' && email=='email@gmail.com'){
+        window.location.href = 'banking.html';
     }
 
-    names.value= '';
+    // names.value= '';
 })
