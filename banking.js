f67aa11e3129f8887af48989f8dd70ad4b566dc9
shafik720 <shafirkasel5@gmail.com>
2022-08-14 13:26:07 +0600

    sdfsf

--- banking.js
+++ banking.js
@@ -5,7 +5,6 @@ function addAmount(inputType,errorField,showField){
 
     let depositShows = document.getElementById(showField+'-show');
     let depositShow = parseFloat(depositShows.innerText)
-    console.log(depositShow);
     
     let errors = document.getElementById(errorField+'-error');
     if(getInputValues.value == ''){
@@ -17,12 +16,11 @@ function addAmount(inputType,errorField,showField){
         errors.innerText = 'Words are not accepted. Input only Number';  
     }else if(getInputValues.value<=0){
         errors.style.display= 'block';
-        errors.innerText = 'Minus value is not accepted !';
+        errors.innerText = 'Minus value or Zero is not accepted !';
     }else if(isNaN(getInputValue)!=true){
         errors.style.display= 'none';
         let depositBalance = depositShow + getInputValue;
         depositShows.innerText = depositBalance;
-        console.log(depositBalance);
     }
 
 
